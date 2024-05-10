dc112b40fb2855c19cae5139de0904eb5611637a
MakeLikePaperrr <de.hoop.stephan@gmail.com>
2024-05-10 16:55:05 +0200

    Add simple solver using Eigen library and add small integrated test

---
+++ src/SimpleReservoirSimulator/Solver/Solver.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <vector>
+#include <Eigen/Sparse>
+
+class Solver
+{
+public:
+	Solver(int numberOfUnknowns);
+	/// <summary>
+	/// Contains the solution to Ax = b (A = connectivity matrix with transmissibility as coefficient, and b is source terms, while x = pressure)
+	/// </summary>
+	std::vector<double> Solution;
+
+	void SetConstantPressureInCell(double constantPressure, int cellNumber);
+	void FillInnerDomainLinearSystem(double transmissibilityCoefficient);
+	void FillInnerDomainLinearSystem(std::vector<double>& transmissibilityCoefficients);
+	void SetStandardBoundaryConditions(double pressureLeft, double pressureRight);
+
+	void SolveLinearSystem();
+
+private:
+	int const _numberOfUnknowns;
+	Eigen::SparseMatrix<double> _linearSystem;
+	Eigen::VectorXd _rightHandSide;
+	Eigen::VectorXd _pressure;
+
+	void SetupLinearSystem();
+	void FillRowLinearSystem(double& transm, int& ithRow);
+	void FillRowLinearSystem(double& transmLeft, double& transmRight, int& ithRow);
+};
+
